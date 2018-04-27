e171d88f619f50a45c879e4206f37a0bf752de16
Nguyen Tuan Anh <ntanh0794@gmail.com>
2018-04-27 17:24:56 +0700

    fix route extend master view

--- resources/views/importExport.blade.php
+++ resources/views/importExport.blade.php
@@ -1,19 +1,7 @@
-<html lang="en">
-<head>
-	<title>Export CSV</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" >
-</head>
-<body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<a class="navbar-brand" href="#">Export in Excel and CSV</a>
-			</div>
-		</div>
-	</nav>
+@extends('voyager::master')
+@section('content')
 	<div class="container">
-		<a href="{{ URL::to('downloadExcel/xls') }}"><button class="btn btn-success">Download Excel xls</button></a>
-		<a href="{{ URL::to('downloadExcel/csv') }}"><button class="btn btn-success">Download CSV</button></a>
+		<a href="{{ URL::to('admin/downloadExcel/xls') }}"><button class="btn btn-success">Download Excel xls</button></a>
+		<a href="{{ URL::to('admin/downloadExcel/csv') }}"><button class="btn btn-success">Download CSV</button></a>
 	</div>
-</body>
-</html>
+@endsection
